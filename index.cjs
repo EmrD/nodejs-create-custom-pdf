9c75d35fd99709753b4c447a499b4a9c52cd35de
emrd434 <emrd434@gmail.com>
2024-07-27 18:04:01 +0000

    modified:   index.cjs

--- index.cjs
+++ index.cjs
@@ -1,27 +1,36 @@
 const express = require('express');
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
+app.use(cors()); 
 app.use(express.json());
 
+// PDF oluşturma endpoint'i
 app.post('/create-pdf', (req, res) => {
-  const { title, content } = req.body;
-  const doc = new PDFDocument();
+    const { title, content } = req.body;
 
-  const filename = 'output.pdf';
-  doc.pipe(fs.createWriteStream(filename));
+    // Yeni PDF dökümanı oluştur
+    const doc = new PDFDocument();
+    
+    // PDF'i bir dosyaya yaz
+    const filename = `output.pdf`;
+    doc.pipe(fs.createWriteStream(filename));
 
-  doc.fontSize(25).text(title, 100 , 100); 
-  doc.fontSize(12).text(content, 100, 200);
+    // PDF içeriğini doldur
+    doc.fontSize(25).text(title, 100, 100);
+    doc.fontSize(12).text(content, 100, 150);
 
-  doc.end();
+    // PDF'i kapat ve yazmayı bitir
+    doc.end();
 
-  doc.pipe(res);
+    // PDF'i döndür
+    doc.pipe(res);
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
